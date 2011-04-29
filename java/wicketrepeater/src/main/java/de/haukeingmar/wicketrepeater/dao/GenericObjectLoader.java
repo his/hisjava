b51fd1fd272804770982be0092bedaf98073ea61
haukeingmar@gmail.com <haukeingmar@gmail.com@df314781-31d0-a928-3d47-e9952d84d4cc>
2011-04-29 18:20:41 +0000

    * blush * Correcting the last commit, missing changes

--- java/wicketrepeater/src/main/java/de/haukeingmar/wicketrepeater/dao/GenericObjectLoader.java
+++ java/wicketrepeater/src/main/java/de/haukeingmar/wicketrepeater/dao/GenericObjectLoader.java
@@ -11,8 +11,17 @@ import javax.persistence.Parameter;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+
 import de.haukeingmar.wicketrepeater.model.HasId;
 
+/**
+ * The GenericObjectLoader is used as a basic infrastructure for the generic {@link IDataProvider} implementation.
+ * 
+ * Its task is to load and count domain objects (defined by implementing {@link HasId}) via named queries or dynamically
+ * built JPQL queries.
+ * 
+ */
 public class GenericObjectLoader {
 
 	@PersistenceContext
@@ -37,7 +46,21 @@ public class GenericObjectLoader {
 		if (filter != null) {
 			queryString = queryString + " where " + filter;
 		}
+
+		return count(queryString, parameterMap);
+	}
+
+	public <T extends HasId> long count(final String queryString, final Map<String, Object> parameterMap) {
 		Query q = entityManager.createQuery(queryString);
+
+		fillParameters(q, parameterMap);
+
+		return (Long) q.getSingleResult();
+	}
+
+	public <T extends HasId> long countWithNamedQuery(final Class<T> clazz, final String namedQueryName,
+			final Map<String, Object> parameterMap) {
+		Query q = entityManager.createNamedQuery(namedQueryName);
 
 		fillParameters(q, parameterMap);
 
@@ -46,10 +69,10 @@ public class GenericObjectLoader {
 
 	private void fillParameters(final Query query, final Map<String, Object> parameterMap) {
 		if (parameterMap != null) {
-         Set<String> parameterNames = new HashSet<String>();
-         for (Parameter p : query.getParameters()) {
-            parameterNames.add(p.getName());
-         }
+			Set<String> parameterNames = new HashSet<String>();
+			for (Parameter p : query.getParameters()) {
+				parameterNames.add(p.getName());
+			}
 			for (String aName : parameterMap.keySet()) {
 				if (parameterNames.contains(aName)) {
 					query.setParameter(aName, parameterMap.get(aName));
@@ -83,15 +106,42 @@ public class GenericObjectLoader {
 			queryString = queryString + " order by " + orderClause;
 		}
 
+		return getList(clazz, queryString, first, count, parameterMap);
+
+	}
+
+	public <T extends HasId> List<T> getList(final Class<T> clazz, final String queryString, final int first,
+			final int count, final Map<String, Object> parameterMap) {
 		Query q = entityManager.createQuery(queryString).setFirstResult(first).setMaxResults(count);
 
 		fillParameters(q, parameterMap);
 
 		return q.getResultList();
+	}
 
+	public <T extends HasId> List<T> getList(final Class<T> clazz, final String queryString,
+			final Map<String, Object> parameterMap) {
+		Query q = entityManager.createQuery(queryString);
+
+		fillParameters(q, parameterMap);
+
+		return q.getResultList();
+	}
+
+	public <T extends HasId> List<T> getListWithNamedQuery(final Class<T> clazz, final String queryName,
+			final int first, final int count, final Map<String, Object> parameterMap) {
+		Query q = entityManager.createNamedQuery(queryName).setFirstResult(first).setMaxResults(count);
+
+		fillParameters(q, parameterMap);
+
+		return q.getResultList();
 	}
 
 	public <T extends HasId> T load(final Class<T> clazz, final Serializable key) {
-		return entityManager.find(clazz, key);
+		if (key != null) {
+			return entityManager.find(clazz, key);
+		} else {
+			return null;
+		}
 	}
 }
